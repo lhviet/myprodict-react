aa0ea403fdbeca01e7c9da9dc538710598c2dc64
viet <lhviet88@gmail.com>
2019-05-02 21:46:28 +0900

    SpeechRecognition

--- src/2_containers/pages/PageReadAloud/index.tsx
+++ src/2_containers/pages/PageReadAloud/index.tsx
@@ -6,77 +6,53 @@ import { IStoreState } from '^/types';
 import PageLayout from '../_PageLayout';
 
 import styles from './styles.module.scss';
-import axios from 'axios';
-import {HOST} from '^/app-configs';
 
 interface Props {
 }
 
 interface State {
+  isSpeechAvailable: boolean;
   isRecording: boolean;
-  isMicAvailable: boolean;
 }
 
-class PageReadAloud extends React.Component<Props, State> {
-  audioChunks: any[] = [];
-  mediaRecorder: MediaRecorder | undefined;
-  audioBlob: Blob | undefined;
+class PagePronouncing extends React.Component<Props, State> {
+  speechRecognition: SpeechRecognition | undefined;
 
   constructor(props: Props, context: any) {
     super(props, context);
 
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then((stream) => {
-        this.mediaRecorder = new MediaRecorder(stream);
-        this.mediaRecorder.ondataavailable = this.onRecorderDataAvailable;
-        this.mediaRecorder.onstart = this.onRecorderStart;
-        this.mediaRecorder.onstop = this.onRecorderStop;
-        this.setState({ isMicAvailable: true });
-      });
+    let isSpeechAvailable: boolean = true;
+
+    if (!('webkitSpeechRecognition' in window)) {
+      isSpeechAvailable = false;
+    } else {
+      this.speechRecognition = new webkitSpeechRecognition() as SpeechRecognition;
+      this.speechRecognition.continuous = true;
+      this.speechRecognition.interimResults = true;
+    }
 
     this.state = {
-      isMicAvailable: false,
+      isSpeechAvailable,
       isRecording: false,
     };
   }
 
-  onRecorderDataAvailable = (event: BlobEvent) => this.audioChunks.push(event.data);
-  onRecorderStart = () => {
-    this.audioChunks = [];
-    this.setState({ isRecording: true });
-  }
-  onRecorderStop = () => {
-    this.audioBlob = new Blob(this.audioChunks, {type: 'audio/mpeg-3'});
-    this.setState({ isRecording: false });
-  }
-
   onStartRecording = () => {
-    if (this.mediaRecorder && !this.state.isRecording) {
-      this.mediaRecorder.start();
+    if (this.speechRecognition && !this.state.isRecording) {
+      this.speechRecognition.start();
+      this.setState({ isRecording: true });
     }
   }
   onStopRecording = () => {
-    if (this.mediaRecorder && this.state.isRecording) {
-      this.mediaRecorder.stop();
+    if (this.speechRecognition && this.state.isRecording) {
+      this.speechRecognition.stop();
+      this.setState({ isRecording: false });
     }
   }
 
   render() {
-    const { isMicAvailable, isRecording }: State = this.state;
-    const playAudio = () => {
-      if (this.audioBlob) {
-        const audioUrl: string = URL.createObjectURL(this.audioBlob);
-        const audio = new Audio(audioUrl);
-        audio.play().then(() => console.log('Play recorded audio now ~~~'));
-
-        const bodyFormData = new FormData();
-        bodyFormData.set('data', this.audioBlob);
-        axios.post(HOST.api.getUrl('/api/speech'), bodyFormData);
-      }
-
-    };
-
-    const microphoneIcon: React.ReactNode = isMicAvailable ? (
+    const { isSpeechAvailable, isRecording }: State = this.state;
+    const microphoneIcon: React.ReactNode = isSpeechAvailable ? (
       isRecording ? (
         <i
           className={'fa fa-microphone fa-2x text-muted an-pulse-btn ' + styles.micBtn}
@@ -88,15 +64,13 @@ class PageReadAloud extends React.Component<Props, State> {
     ) : (
       <i className={'fa fa-microphone-slash fa-2x'} />
     );
-
     return (
       <PageLayout>
-        <div className={styles.pageReadAloud}>
+        <div className={styles.pagePronouncing}>
           <textarea>
             Something inside the textarea here...
           </textarea>
           {microphoneIcon}
-          <button className={'btn btn-primary'} onClick={playAudio}>Save</button>
         </div>
       </PageLayout>
     );
@@ -109,4 +83,4 @@ const mapStateToProps = (state: IStoreState) => ({
 const mapDispatchToProps = () => ({
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(PageReadAloud);
+export default connect(mapStateToProps, mapDispatchToProps)(PagePronouncing);
